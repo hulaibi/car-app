93975c39760d13b356d3425d94e6e562ff3a4664
thaistrok <thaistrok@gmail.com>
2025-05-19 18:11:19 +0300

    authRoute.js

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -8,8 +8,14 @@ router.get('/sign-up', (req, res) => {
 })
 
 router.post('/sign-in', authController.signInUser)
+router.get('/sign-in', (req, res) => {
+  res.render('./auth/sign-in.ejs')
+})
 
 router.get('/sign-out', authController.signOutUser)
+router.get('/sign-out', (req, res) => {
+  res.render('./auth/sign-out.ejs')
+})
 
 router.put('/:id', authController.updatePassword)
 
