9cc8e687c2a803fe4ed5a86851f850c8166472b4
HasanNaser <hassan.alhulaibi@gmail.com>
2025-05-22 10:46:26 +0300

    neeed to work today

--- routes/carRouter.js
+++ routes/carRouter.js
@@ -1,26 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const carController = require("../controllers/carController.js");
-const Car = require("../models/Car.js");
 
 router.get("/new", (req, res) => {
-  res.render("cars/new", { user: req.session.user });
-});
+  res.render("cars/new", { user: req.session.user }); 
 
-router.get("/edit/:id", async (req, res) => {
-  let car = await Car.findById(req.params.id);
-  if (!car) {
-    return res.render("cars/all");
-  }
-  res.render("cars/edit", { car });
 });
 
 router.post("/new", carController.addCar);
 router.get("/all", carController.getAllCars);
-router.get("/:id", carController.getCarById);
-router.post("/update", carController.updateCarById); // dont works with put
-
+router.get("/:id",carController.getCarById);
+router.put("/update", carController.updateCarById);
 router.get("/new", carController.newCar);
-router.post("/delete", carController.deleteCarById); // dont works with put
+router.delete("/delete", carController.deleteCarById);
 
 module.exports = router;
